d5dc8831899ec610623adfa41844b03f9687370c
wang stephen <551619855@qq.com>
2016-12-21 13:35:27 +0800

    ````

--- DeepFusing/UKF.py
+++
@@ -1,5 +0,0 @@
-# -*- coding:utf-8 -*-
-# carete by steve at  2016 / 12 / 19　11:52
-import numpy as np
-import scipy as sp
-
---
+++ OPENSHOE/UKF.py
@@ -0,0 +1,23 @@
+# -*- coding:utf-8 -*-
+# carete by steve at  2016 / 12 / 19　11:52
+'''
+This module is use to compute imu path through ukf method.
+'''
+import numpy as np
+import scipy as sp
+
+import math
+
+from OPENSHOE.Setting import settings
+
+
+class UKFIns(object):
+    '''
+
+    '''
+    def __init__(self,settings):
+        '''
+
+        '''
+        self.para
+
